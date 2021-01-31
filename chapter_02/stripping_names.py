ce624268bc9af59c22c06c1e249f79952a3408b9
Blanca-7 <38846733+Blanca-7@users.noreply.github.com>
2021-01-31 19:00:59 +0100

    Create stripping_names.py

---
+++ chapter_02/stripping_names.py
@@ -0,0 +1,9 @@
+name = "\tVanessa\n"
+print("Unmodified:\n")
+print(name)
+print("lstrip:\n")
+print(name.lstrip())
+print("rstrip:\n")
+print(name.rstrip())
+print("strip both sides:\n")
+print(name.strip())
