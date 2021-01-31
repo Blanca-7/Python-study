d473ade571a547a3ef7dba1ebb135986b3942351
Blanca-7 <38846733+Blanca-7@users.noreply.github.com>
2021-01-31 18:26:41 +0100

    Create name_cases.py

---
+++ chapter_02/name_cases.py
@@ -0,0 +1,4 @@
+name = "Blanca"
+print("The following shows " + name.lower() + " in all lowercase")
+print("The following shows " + name.upper() + " in all uppercase")
+print("The following shows " + name.title() + " in title case")
