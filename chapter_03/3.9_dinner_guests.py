6a26b6fbc1120e14e1534a86c024d35bfb2bff36
Blanca-7 <38846733+Blanca-7@users.noreply.github.com>
2021-02-05 11:48:45 +0100

    Create 3.9_dinner_guests.py

---
+++ chapter_03/3.9_dinner_guests.py
@@ -0,0 +1,9 @@
+guests = ["Dr. Einstein","Bruce Lee","Elon Musk"]
+invitation = "I'd like to invite you to my dinner Party"
+print(f"Dear {guests[0]}, {invitation}")
+print(f"Dear {guests[1]}, {invitation}")
+print(f"Dear {guests[2]}, {invitation}")
+
+print(f"\nI'm inviting {len(guests)} guests")
+
+
