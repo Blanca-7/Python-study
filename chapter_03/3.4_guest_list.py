5ef5983e66213001080c61aa2e739d767ba3143b
Blanca-7 <38846733+Blanca-7@users.noreply.github.com>
2021-02-05 11:45:50 +0100

    Create 3.4_guest_list.py

---
+++ chapter_03/3.4_guest_list.py
@@ -0,0 +1,8 @@
+guests = ["Dr. Einstein","Bruce Lee","Elon Musk"]
+invitation = "I'd like to invite you to my dinner Party"
+print(f"Dear {guests[0]}, {invitation}")
+print(f"Dear {guests[1]}, {invitation}")
+print(f"Dear {guests[2]}, {invitation}")
+
+#alternative way with concatenation: 
+#print("Dear " + guests[1] + ", " + invitation)
